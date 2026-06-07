f515975c49a483c99e64d16ffeac4a797e113214
Abhishek Shukla <abhishekshukla8097@gmail.com>
2026-06-07 22:04:34 +0530

    create instance for client and server

---
+++ real-chat-app/lib/socket.js
@@ -0,0 +1,33 @@
+import { io } from 'socket.io-client';
+
+const SOCKET_URL = 'http://localhost:3000';
+
+let socket = null;
+
+export const getSocket = () => {
+  if (!socket) {
+    socket = io(SOCKET_URL, {
+      autoConnect: false,
+      reconnection: true,       
+      reconnectionAttempts: 5,
+      reconnectionDelay: 1000,
+    });
+  }
+  return socket;
+};
+
+export const connectSocket = (username) => {
+  const s = getSocket();
+  if (!s.connected) {
+    s.connect();
+    s.emit('user-join', username);
+  }
+  return s;
+};
+
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
